15e3ba4ca55afefec0b4e2be76f000a17a5f2e47
Arlene Andrews <kara_arlene@yahoo.com>
2018-12-26 16:45:56 -0700

    Adding pakages, and creating a gitignore

--- timer.js
+++ timer.js
@@ -1,20 +1,21 @@
 const increaseCounter = () => {
-    <button onClick={his.setState({counter: this.state.counter - 1})}>Down</button>;
-   } 
-   const decreaseCounter = () => {
-     <button onClick={his.setState({counter: this.state.counter - 1})}>Down</button>;
-   }
-   
-   class App extends React.Component {
-     state = {counter: 0}
-   
-     render() {
-       return <div>
-         {this.state.counter}
-         <p />
-         <increaseCounter />
-         <decreaseCounter />
-       </div>;
-       }
-   
-   ReactDOM.render(<App />, document.querySelector('#root'));
+  <button onClick={this.setState({counter: this.state.counter - 1})}>Down</button>;
+} 
+
+const decreaseCounter = () => {
+  <button onClick={this.setState({counter: this.state.counter - 1})}>Down</button>;
+}
+
+class App extends React.Component {
+  state = {counter: 0}
+
+  render() {
+    return <div>
+      {this.state.counter}
+      <p />
+      <increaseCounter />
+      <decreaseCounter />
+    </div>;
+  }
+}
+ReactDOM.render(<App />, document.querySelector('#root'));
