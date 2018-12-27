85eaa1264c6eda26ede4143e66c0918e0493d0ca
Arlene Andrews <kara_arlene@yahoo.com>
2018-12-26 20:21:43 -0700

    well, at least the fail is the same

--- tests/button.test.js
+++ tests/button.test.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactTestUtils from 'react-dom/test-utils'; 
+import TestRenderer from 'react-test-renderer';
 
 describe('Buttons', () => {
     describe('Up button', () => {
- 
-        it('Displays on screen', () => {
+        
+       /*  it('Displays on screen', () => {
             const component = ReactTestUtils.renderIntoDocument(<increaseCounter />);    
             var h2 = ReactTestUtils.findRenderedDOMComponentWithTag(
              component, 'button'
             )
-            })
-       
+            }) */
+            it('Increases count by one', () => {
+                const testRenderer = TestRenderer.create(<App />);
+                const up = this.button;
+                ReactTestUtils.Simulate.click(up);
+                })
     })
 })
