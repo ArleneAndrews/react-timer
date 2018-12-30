cfd00d9d815718bcb8e9b4bd179e0cda03419a7f
Arlene Andrews <kara_arlene@yahoo.com>
2018-12-30 12:42:43 -0700

    More fiddle - I should be a virtoso by now.

--- tests/button.test.js
+++ tests/button.test.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import {renderIntoDocument} from 'react-testing-library';
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import {renderIntoDocument} from 'react-testing-library';
 import App from '../timer';
 
 // afterEach(cleanup)
 
-describe('Buttons', () => {
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+/* describe('Buttons', () => {
     describe('Up button', () => {
         it('Displays on screen', () => {
         renderIntoDocument(<App />);
         expect(getByText('My Counter')).toBeTruthy();
         });
     })
-})
+}) */
