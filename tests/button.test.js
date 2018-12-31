6ac3102dd4aded84b6d816ddafc0b09def90c0fd
Arlene Andrews <kara_arlene@yahoo.com>
2018-12-30 20:21:46 -0700

    updates- again.Still not working.

--- tests/button.test.js
+++ tests/button.test.js
@@ -1,21 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//import {renderIntoDocument} from 'react-testing-library';
-import App from '../timer';
-
-// afterEach(cleanup)
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from '../src/timer';
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-  });
-
-  
-/* describe('Buttons', () => {
-    describe('Up button', () => {
-        it('Displays on screen', () => {
-        renderIntoDocument(<App />);
-        expect(getByText('My Counter')).toBeTruthy();
-        });
-    })
-}) */
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+})
